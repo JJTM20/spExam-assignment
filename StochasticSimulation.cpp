9284d377e5993ccdf00afb0c0b1e2429381f25cf
JJTM20 <jjtm20@student.aau.dk>
2024-05-20 14:07:47 +0200

    Algorithm: 8. TODO: Implement lookup table

--- StochasticSimulation.cpp
+++ StochasticSimulation.cpp
@@ -4,27 +4,51 @@
 
 #include "StochasticSimulation.h"
 
-//Prototypes
-const std::chrono::duration<double, std::milli> ComputeDelay();
+//Global variables
+auto global_state = GlobalState();
 
-void StochasticSimulation::RunSimulation(std::vector<Reaction> reaction_set, std::chrono::time_point<std::chrono::high_resolution_clock> end_time, GlobalState state) {
-    const std::chrono::time_point<std::chrono::high_resolution_clock> t = std::chrono::high_resolution_clock::now();
-    while (t <= end_time){
+//Prototypes
+const Reaction FindSmallestDelayReaction();
+
+void StochasticSimulation::RunSimulation(std::vector<Reaction> reaction_set, double end_time, GlobalState state) {
+    //const std::chrono::time_point<std::chrono::high_resolution_clock> t = std::chrono::high_resolution_clock::now();
+
+    while (global_state.GetCurrentTime() <= end_time){
         for (auto r : reaction_set) {
-            auto delay = ComputeDelay(); //TODO: Implement so that the delay is tied to the reaction object
+            auto delay = ComputeReactionTime(r); //TODO: Implement so that the delay is tied to the reaction object
+            r.set_rate_parameter(delay);
+        }
+
+        // Pick reaction with shortest delay (reaction time)
+        auto min_delay_reaction = FindSmallestDelayReaction();
+
+        global_state.AddTime(min_delay_reaction.get_current_rate_parameter());
+        for (auto q : min_delay_reaction.get_reactants()) {
+            //Implement lookup table
         }
     }
 }
 
-const std::chrono::duration<double, std::milli> StochasticSimulation::ComputeReactionTime(Reaction reaction){
+const double StochasticSimulation::ComputeReactionTime(Reaction reaction){
     std::vector<Molecule> reactants = reaction.get_reactants();
     double total_amount_of_reactants;
     for (auto m:reactants){
         total_amount_of_reactants += m.get_current_amount();
     }
     double r_delay = reaction.get_current_rate_parameter() * total_amount_of_reactants;
-    return new std::chrono::durationr_delay;
+    return r_delay;
 }
+
+const Reaction FindSmallestDelayReaction(){
+    auto min_delay_reaction = Reaction();
+    min_delay_reaction.set_rate_parameter(std::numeric_limits<double>::infinity());
+    for (auto r : global_state.reactions) {
+        if (r.get_current_rate_parameter() < min_delay_reaction.get_current_rate_parameter()){
+            min_delay_reaction = r;
+        }
+    }
+}
+
 
 long RandomNumberGen(double delay){
     std::random_device rd;
--- StochasticSimulation.h
+++ StochasticSimulation.h
@@ -22,13 +22,18 @@ public:
 
 class GlobalState{
 private:
+    double time = 0;
     std::list<std::pair<Molecule, int>> reactant_amounts;
+public:
+    std::list<Reaction> reactions;
+    void AddTime(double time_to_add){time += time_to_add;}
+    double GetCurrentTime(){return time;}
 };
 
 class StochasticSimulation {
 public:
-    void RunSimulation(std::vector<Reaction> reaction_set, std::chrono::time_point<std::chrono::high_resolution_clock> end_time, GlobalState state);
-    const std::chrono::duration<double, std::milli> ComputeReactionTime(Reaction reaction);
+    void RunSimulation(std::vector<Reaction> reaction_set, double end_time, GlobalState state);
+    const double ComputeReactionTime(Reaction reaction);
 };
 
 
