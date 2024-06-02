33bf87357e4fa3e725d0abc7db8f6b5537358eb7
JJTM20 <jjtm20@student.aau.dk>
2024-06-02 16:12:23 +0200

    Small graph rework (-molecule_index..)

---
+++ Grapher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jjtor on 02/06/2024.
+//
+
+#include "Grapher.h"
---
+++ Grapher.h
@@ -0,0 +1,16 @@
+//
+// Created by jjtor on 02/06/2024.
+//
+
+#ifndef EKSAMENSPROJEKT_GRAPHER_H
+#define EKSAMENSPROJEKT_GRAPHER_H
+
+
+
+class Grapher {
+
+};
+
+
+
+#endif //EKSAMENSPROJEKT_GRAPHER_H
