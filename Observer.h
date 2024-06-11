f883525280fa6bbde1caecf0cf5cd99a7b4f051a
JJTM20 <jjtm20@student.aau.dk>
2024-06-11 23:01:29 +0200

    Commit b4 everyting breaks

---
+++ Observer.h
@@ -0,0 +1,14 @@
+//
+// Created by jjtor on 11/06/2024.
+//
+
+#ifndef EKSAMENSPROJEKT_OBSERVER_H
+#define EKSAMENSPROJEKT_OBSERVER_H
+
+
+class Observer {
+
+};
+
+
+#endif //EKSAMENSPROJEKT_OBSERVER_H
